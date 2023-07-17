c7df86b8058d20af11bdbcef72a04fc02e8494e6
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2023-07-17 09:36:51 +0530

    Create Modular Exponentiation.cpp

---
+++ Modular Exponentiation.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+int modularExponentiation(int x, int n, int m) {
+	// Write your code here.
+	 int res=1;
+ while(n>0){
+    if(n&1){
+        //odd
+        res=(1LL*(res)*(x)%m)%m;
+
+    }
+    x=(1LL*(x)%m * (x)%m)%m;
+    n=n>>1;
+
+ }
+ return res;
+ 
+}
